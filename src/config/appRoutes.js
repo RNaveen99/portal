b25051087d88c9a7f2a18a9d58d81f50303d1325
Naveen Rohilla <naveenrohilla99@gmail.com>
2019-12-16 23:00:14 +0530

    route for findUsers added

--- src/config/appRoutes.js
+++ src/config/appRoutes.js
@@ -2,17 +2,15 @@ const debug = require('debug')('app:appRoutes');
 const authRouter = require('../routes/authRoutes')();
 const eventRouter = require('../routes/eventRoutes')();
 const friendRouter = require('../routes/friendRoutes')();
+const userRouter = require('../routes/userRoutes')();
 
 const appRoutes = (app) => {
   app.use('/auth', authRouter);
   app.use('/events', eventRouter);
   app.use('/friends', friendRouter);
+  app.use('/findUsers', userRouter);
   app.get('/', (req, res) => {
     res.redirect('/events');
-  });
-  app.get('/ajax', (req, res) => {
-    const temp = { name: 'pawan' };
-    res.jsonp(temp);
   });
 };
 
