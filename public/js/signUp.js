028b0300b232c77d772313a9f12571aa5c1628bf
Naveen Rohilla <naveenrohilla99@gmail.com>
2019-12-16 19:23:43 +0530

    validation bug fixes

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const elems = document.querySelectorAll('.autocomplete');
-  const instances = M.Autocomplete.init(elems, {
+  M.Autocomplete.init(document.querySelectorAll('.autocomplete'), {
     data: {
       'Acharya Narendra Dev College': null,
       'Atma Ram Sanatan Dharma College': null,
@@ -26,26 +25,146 @@ document.addEventListener('DOMContentLoaded', () => {
       'Ramjas College': null,
       'Shyama Prasad Mukherji College': null,
       'Sri Guru Tegh Bahadur Khalsa College': null,
-      
     },
   });
 });
+
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('#number').addEventListener('change', (e) => {
-    e.target.value = e.target.value.substring(0, 10);
-  });
-  document.querySelector('#name').addEventListener('change', (e) => {
-    let name = e.target.value.trim().substring(0, 30);
-    const { length } = name;
-    let flag;
-    for (let i = 0; i < length; i++) {
-      flag = /[^A-Za-z ]/.test(name);
-      if (flag) {
-        name = name.replace(/[^A-Za-z ]/, '');
-      } else {
-        break;
-      }
+  const name = document.querySelector('#name');
+  const nameHelper = document.querySelector('.name-helper');
+  const college = document.querySelector('#college');
+  const collegeHelper = document.querySelector('.college-helper');
+  const number = document.querySelector('#number');
+  const numberHelper = document.querySelector('.number-helper');
+  const email = document.querySelector('#email');
+  const emailHelper = document.querySelector('.email-helper');
+  const password = document.querySelector('#password');
+  const passwordHelper = document.querySelector('.password-helper');
+  const submitButton = document.querySelector('button[type="submit"]');
+
+  function setVaidationIcon(field, icon) {
+    document.querySelector(`.${field}-icon`).textContent = icon;
+  }
+
+  function validateName() {
+    const nameValue = name.value;
+    let msg = '';
+    const flag1 = /^([A-Za-z])([A-Za-z ]){0,28}([A-Za-z])$/.test(nameValue);
+    if (!flag1) {
+      msg += 'Only Alphabets and Spaces in-between.(Min 2 alphabets.)';
     }
-    e.target.value = name;
-  });
+    let flag2 = true;
+    if (nameValue.length > 30) {
+      flag2 = false;
+      msg += 'Max 30 characters.';
+    }
+    nameHelper.textContent = msg;
+    const flag = flag1 && flag2;
+    if (flag) {
+      setVaidationIcon('name', 'check');
+    } else {
+      setVaidationIcon('name', 'clear');
+    }
+    return flag;
+  }
+
+  function validateCollege() {
+    const collegeValue = college.value;
+    let msg = '';
+    const flag1 = /^([A-Za-z])([A-Za-z ]){0,68}([A-Za-z])$/.test(collegeValue);
+    if (!flag1) {
+      msg += 'Only Alphabets and Spaces in-between.(Min 2 alphabets.)';
+    }
+    let flag2 = true;
+    if (collegeValue.length > 70) {
+      flag2 = false;
+      msg += 'Max 70 characters.';
+    }
+    collegeHelper.textContent = msg;
+    const flag = flag1 && flag2;
+    if (flag) {
+      setVaidationIcon('college', 'check');
+    } else {
+      setVaidationIcon('college', 'clear');
+    }
+    return flag;
+  }
+  function validateNumber() {
+    const numberValue = number.value;
+    let msg = '';
+    const flag1 = /[^0-9]/.test(numberValue);
+    if (flag1) {
+      msg += 'Numbers only. ';
+    }
+    let flag2 = true;
+    if (numberValue.length > 10) {
+      flag2 = false;
+      msg += 'Max 10 characters.'
+    } else if (numberValue.length < 10) {
+      flag2 = false;
+      msg += 'Min 10 characters.';
+    }
+    numberHelper.textContent = msg;
+    const flag = !flag1 && flag2;
+    if (flag) {
+      setVaidationIcon('number', 'check');
+    } else {
+      setVaidationIcon('number', 'clear');
+    }
+    return flag;
+  }
+
+  function validateEmail() {
+    const emailValue = email.value;
+    let msg = '';
+    const flag = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(emailValue);
+    if (!flag) {
+      msg += 'Enter a proper email address.';
+    }
+    emailHelper.textContent = msg;
+    if (flag) {
+      setVaidationIcon('email', 'check');
+    } else {
+      setVaidationIcon('email', 'clear');
+    }
+    return flag;
+  }
+  function validatePassword() {
+    const passwordValue = password.value;
+    let msg = '';
+    const flag1 = / /.test(passwordValue);
+    if (flag1) {
+      msg += 'Spaces are not allowed.';
+    }
+    let flag2 = true;
+    if (passwordValue.length < 5) {
+      flag2 = false;
+      msg += ' Minimum length should be 5';
+    }
+    passwordHelper.textContent = msg;
+    const flag = !flag1 && flag2;
+    if (flag) {
+      setVaidationIcon('password', 'check');
+    } else {
+      setVaidationIcon('password', 'clear');
+    }
+    return flag;
+  }
+
+  function validateFields(e) {
+    const flag1 = validateName();
+    const flag2 = validateCollege();
+    const flag3 = validateNumber();
+    const flag4 = validateEmail();
+    const flag5 = validatePassword();
+    if (!(flag1 && flag2 && flag3 && flag4 && flag5)) {
+      e.preventDefault();
+    }
+  }
+  name.addEventListener('keyup', validateName);
+  college.addEventListener('keyup', validateCollege);
+  number.addEventListener('keyup', validateNumber);
+  email.addEventListener('keyup', validateEmail);
+  password.addEventListener('keyup', validatePassword);
+  submitButton.addEventListener('click', validateFields);
 });
--- src/controllers/helpers/validators.js
+++ src/controllers/helpers/validators.js
@@ -3,20 +3,20 @@ const { findUserByEmail } = require('./mongo')();
 
 module.exports.validator = [
   body('name')
-    .not().isEmpty().withMessage('Name cannnot be empty')
-    .matches(/^[a-zA-Z ]{2,30}$/).withMessage('Only Alphabets with max 30 characters.')
-    .trim(),
-  body('college')
-    .not().isEmpty().withMessage('College name cannnot be empty')
-    .matches(/^[a-zA-Z ]{2,50}$/).withMessage('Only Alphabets')
-    .trim(),
-  body('number')
-    .not().isEmpty().withMessage('Number cannot be empty')
     .trim()
+    .not().isEmpty().withMessage('Name cannnot be empty')
+    .matches(/^([A-Za-z])([A-Za-z ]){0,28}([A-Za-z])$/).withMessage('Only Alphabets with min 2 & max 30 characters and spaces in-between (if any).'),
+  body('college')
+    .trim()
+    .not().isEmpty().withMessage('College name cannnot be empty')
+    .matches(/^([A-Za-z])([A-Za-z ]){0,68}([A-Za-z])$/).withMessage('Only Alphabets with min 2 & max 70 characters and spaces in-between (if any).'),
+  body('number')
+    .trim()  
+    .not().isEmpty().withMessage('Number cannot be empty')
     .matches(/^(\d{10})$/).withMessage('Enter a proper 10-digit mobile number.'),
   body('email')
-    .not().isEmpty().withMessage('Email cannnot be empty')
     .trim()
+    .not().isEmpty().withMessage('Email cannnot be empty')  
     .isEmail().withMessage('Enter a proper email')
     .custom(async (value) => {
       const user = await findUserByEmail(value);
@@ -26,7 +26,6 @@ module.exports.validator = [
     }),
   body('password')
     .not().isEmpty().withMessage('Password cannot be empty')
-    .trim()
     .not().matches(/ /).withMessage('spaces not allowed in password')
     .isLength({ min:5 }).withMessage('Minimum length should be 5'),
 ];
